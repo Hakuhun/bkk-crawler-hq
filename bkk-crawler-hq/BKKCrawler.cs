df8d1381774f2977350ab14ae959ccd9145c10d3
Bakonyi Gergő <bakonyi.gergo.istvan@gmail.com>
2018-10-04 21:40:45 +0200

    Project was created

---
+++ bkk-crawler-hq/BKKCrawler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bkk_crawler_hq
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class BKKCrawler
+    {
+        /// <summary>
+        /// The API key 
+        /// </summary>
+        private readonly string key = "key=bkk-web";
+        /// <summary>
+        /// The base URL of the API
+        /// </summary>
+        private readonly string base_url = "http://futar.bkk.hu/bkk-utvonaltervezo-api/ws/otp/api/where/";
+        /// <summary>
+        /// The used application api, using the apiary's one.
+        /// TODO: May get an app key for this app
+        /// </summary>
+        private readonly string appVersion = "appVersion=apiary-1.0";
+        /// <summary>
+        /// API Version
+        /// </summary>
+        private readonly string version = "version=3";
+
+
+
+
+
+
+    }
+}
---
+++ bkk-crawler-hq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bkk_crawler_hq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
